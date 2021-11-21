2bdc86388efaf1efeec74d850ca66ccb31a6e9ce
MOREL Rodrigue <morel.rodrigue@protonmail.com>
2021-11-21 16:31:32 +0100

    update

--- one.c
+++ one.c
@@ -11,12 +11,12 @@
 
 int main(int argc, char **argv){
   printf("Les tailles de variables en octets : \n");
-  printf("long        : %d\n",sizeof(long));
-  printf("float       : %d\n",sizeof(float));
-  printf("double      : %d\n",sizeof(double));
-  printf("int         : %d\n",sizeof(int));
-  printf("short       : %d\n",sizeof(short));
-  printf("char        : %d\n",sizeof(char));
-  printf("int long    : %d\n",sizeof(int long));
+  printf("char         : %d\n",sizeof(char));
+  printf("short        : %d\n",sizeof(short));
+  printf("int          : %d\n",sizeof(int));
+  printf("long         : %d\n",sizeof(long));
+  printf("int long     : %d\n",sizeof(int long));
+  printf("float        : %d\n",sizeof(float));
+  printf("double       : %d\n",sizeof(double));
   return EXIT_SUCCESS;
 }
